43853a44a937eca15041c79fcb3c9149fa6365da
Artemka007 <kopylovartyom007@gmail.com>
2021-12-11 16:14:19 +0500

    With comments

--- api/mixins.py
+++ api/mixins.py
@@ -1,6 +1,33 @@
 from django.db.models.query import Q
+from rest_framework.generics import GenericAPIView, CreateAPIView
 
+class ProjectAPIView(GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        super().put(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+    
+    def get_response():
+        pass
+    
 class SearchMixin:
+    '''
+    The mixin that return filtered objects.
+    ---
+    For the mixin know, by which fields do
+    filter, view should has two attrs:
+    * `search_fields` - array of fields for filter by query string
+    * `detail_search_fields` - array of fields for filter by query obj
+    ---
+    Use: `objects = self.get_objects(q)` where q is query string or object
+    '''
     def get_objects(self, q):
         if not self.detail_search_fields:
             raise Exception("SearchOffers object has no attribute detail_search_fields")
